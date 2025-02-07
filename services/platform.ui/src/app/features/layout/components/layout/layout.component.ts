92d517ec2ee49f96a5baf2e7e68253698fe02fc8
Amanuel <amanuel.meketa@gmail.com>
2025-02-07 14:44:38 +0300

    Footer and logout complated

--- services/platform.ui/src/app/features/layout/components/layout/layout.component.ts
+++ services/platform.ui/src/app/features/layout/components/layout/layout.component.ts
@@ -5,15 +5,34 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, CommonModule, NzBreadCrumbModule, NzIconModule, NzLayoutModule, NzMenuModule],
+  imports: [RouterOutlet, RouterLink, CommonModule, NzBreadCrumbModule, NzIconModule, NzLayoutModule,
+            NzMenuModule, NzButtonModule, NzDropDownModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
 isCollapsed = false;
   currentYear: number = new Date().getFullYear();
+
+  onPasswordReset(): void {
+    // Implement password reset logic here
+    console.log('Password Reset clicked');
+  }
+
+  onUserProfile(): void {
+    // Implement user profile logic here
+    console.log('User Profile clicked');
+  }
+
+  onLogout(): void {
+    // Implement logout logic here
+    console.log('Logout clicked');
+  }
 }
