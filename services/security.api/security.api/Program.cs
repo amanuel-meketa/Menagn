51d019ac40bdbd5efade739247ec03fc8d89deb3
Amanuel <amanuel.meketa@gmail.com>
2025-04-04 15:10:07 -0700

    swagger Auth2 authentication implmented

--- services/security.api/security.api/Program.cs
+++ services/security.api/security.api/Program.cs
@@ -1,6 +1,6 @@
+using Keycloak.Auth.Api.Extensions;
 using Keycloak.AuthServices.Authentication;
 using Keycloak.AuthServices.Authorization;
-using Microsoft.OpenApi.Models;
 using security.business.Contracts;
 using security.business.Services;
 using security.sharedUtils;
@@ -8,6 +8,8 @@ using security.sharedUtils;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGenWithAuth(builder.Configuration);
+builder.Services.ConfigurApplicationServices();
 builder.Services.AddKeycloakWebApiAuthentication(builder.Configuration);
 builder.Services.AddScoped<IIdentityService, IdentityService>();
 builder.Services.AddScoped<IUserService, UserService>();
@@ -15,7 +17,6 @@ builder.Services.AddScoped<IAccountService, AccountService>();
 builder.Services.AddScoped<IRoleService, RoleService>();
 builder.Services.AddScoped<IEventLogService, EventLogService>();
 builder.Services.AddHttpClient<IdentityService>();
-builder.Services.ConfigurApplicationServices();
 
 /// <summary>
 /// Keycloak roles can be automatically transformed to AspNetCore Roles. This feature is disabled by
@@ -29,7 +30,6 @@ builder.Services.AddAuthorization()
     })
     .AddAuthorizationBuilder();
 
-AddSwaggerDoc(builder.Services);
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAllOrigins",
@@ -56,44 +56,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-void AddSwaggerDoc(IServiceCollection services)
-{
-    services.AddSwaggerGen(c =>
-    {
-        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-        {
-            Description = @"JWT Authorization header using the Bearer scheme. 
-                      Enter 'Bearer' [space] and then your token in the text input below.
-                      Example: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'",
-            Name = "Authorization",
-            In = ParameterLocation.Header,
-            Type = SecuritySchemeType.ApiKey,
-            Scheme = "Bearer"
-        });
-
-        c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-        {
-            {
-                new OpenApiSecurityScheme
-                {
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                    },
-                    Scheme = "oauth2",
-                    Name = "Bearer",
-                    In = ParameterLocation.Header,
-                },
-                new List<string>()
-            }
-        });
-
-        c.SwaggerDoc("v1", new OpenApiInfo
-        {
-            Version = "v1",
-            Title = "Security Service Api",
-        });
-    });
-}
