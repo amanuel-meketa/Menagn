d427fca223305aab15a3de69aa182cdac1c16946
Amanuel <amanuel.meketa@gmail.com>
2025-05-18 03:06:26 -0700

    Security service authentication now uses Standard flow instade of direct access flow

--- services/security.api/security.api/Extensions/ServiceCollectionExtensions.cs
+++ services/security.api/security.api/Extensions/ServiceCollectionExtensions.cs
@@ -1,47 +1,56 @@
 ﻿using Microsoft.OpenApi.Models;
 
 namespace Keycloak.Auth.Api.Extensions;
-public static class ServiceCollectionExtensions
+internal static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddSwaggerGenWithAuth(this IServiceCollection services,IConfiguration configuration)
+    internal static IServiceCollection AddSwaggerGenWithAuth(this IServiceCollection services,IConfiguration configuration)
     {
-        services.AddSwaggerGen(c =>
+        services.AddSwaggerGen(o =>
         {
-            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            o.CustomSchemaIds(id => id.FullName!.Replace('+', '-'));
+
+            o.AddSecurityDefinition("Keycloak", new OpenApiSecurityScheme
             {
-                Description = @"JWT Authorization header using the Bearer scheme. 
-              Enter 'Bearer' [space] and then your token in the text input below.
-              Example: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'",
-                Name = "Authorization",
+                Type = SecuritySchemeType.OAuth2,
+                Flows = new OpenApiOAuthFlows
+                {
+                    AuthorizationCode = new OpenApiOAuthFlow
+                    {
+                        AuthorizationUrl = new Uri(configuration["Authentication:AuthorizationUrl"]!),
+                        TokenUrl = new Uri(configuration["Authentication:TokenUrl"]!),
+                        Scopes = new Dictionary<string, string>
+                        {
+                            { "openid", "openid" },
+                            { "profile", "profile" }
+                        }
+                    }
+                },
                 In = ParameterLocation.Header,
-                Type = SecuritySchemeType.ApiKey,
+
                 Scheme = "Bearer"
             });
 
-            c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-{
-    {
-        new OpenApiSecurityScheme
-        {
-            Reference = new OpenApiReference
+            var securityRequirement = new OpenApiSecurityRequirement
             {
-                Type = ReferenceType.SecurityScheme,
-                Id = "Bearer"
-            },
-            Scheme = "oauth2",
-            Name = "Bearer",
-            In = ParameterLocation.Header,
-        },
-        new List<string>()
-    }
-});
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Id = "Keycloak",
+                            Type = ReferenceType.SecurityScheme
+                        },
+                        In = ParameterLocation.Header,
+                        Name = "Authorization", 
+                        Scheme = "Bearer"
+                    },
+                    new List<string>() 
+                }
+            };
 
-            c.SwaggerDoc("v1", new OpenApiInfo
-            {
-                Version = "v1",
-                Title = "Security Service Api",
-            });
+            o.AddSecurityRequirement(securityRequirement);
         });
+
         return services;
     }
 }
